28cdfad04ba8ae093cf875738debedb9898aff85
Alejandro <a.alcaraz@uniandes.edu.co>
2020-10-22 23:24:07 -0500

    Test y terminacion de RBT y NodeRB

--- src/modeloEstructuraDatos/RBT.java
+++ src/modeloEstructuraDatos/RBT.java
@@ -1,11 +1,12 @@
 package modeloEstructuraDatos;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class RBT<K extends Comparable<K>, V> implements TablaSimbolosOrdenada<K,V>{
 
 	private NodoRB raiz;
-	
+
 
 	@Override
 	public boolean isEmpty() {
@@ -15,8 +16,8 @@ public class RBT<K extends Comparable<K>, V> implements TablaSimbolosOrdenada<K,
 
 	private boolean esRojo(NodoRB x)
 	{
-	if (x == null) return false;
-	return x.esRojo();
+		if (x == null) return false;
+		return x.esRojo();
 	}
 	@Override
 	public V get(K key) {
@@ -58,8 +59,9 @@ public class RBT<K extends Comparable<K>, V> implements TablaSimbolosOrdenada<K,
 
 	@Override
 	public boolean contains(K key) {
-		// TODO Auto-generated method stub
-		return false;
+		V valor = get(key);
+		if(valor==null)return false;
+		return true; 
 	}
 
 	public void put(K key, V valor) {
@@ -74,60 +76,74 @@ public class RBT<K extends Comparable<K>, V> implements TablaSimbolosOrdenada<K,
 		else if (cmp > 0) nodo.setDerecho(put(nodo.darDerecho(), key, valor));
 		else if (cmp ==0) nodo.anadirValorANodo(valor);
 		if (esRojo(nodo.darDerecho()) && !esRojo(nodo.darIzquierdo())) nodo = rotarIzquierdo(nodo);
-		if (esRojo(nodo.darIzquierdo()) && esRojo(nodo.darIzquierdo())) nodo = rotarDerecho(nodo);
+		if (esRojo(nodo.darIzquierdo()) && esRojo(nodo.darIzquierdo().darIzquierdo())) nodo = rotarDerecho(nodo);
 		if (esRojo(nodo.darIzquierdo()) && esRojo(nodo.darDerecho())) flipColores(nodo);
-		
+
 		int nuevoNumeroDeNodosBajoEl = size(nodo.darIzquierdo()) + size(nodo.darDerecho()) + 1;
 		nodo.establecerNumNodosBajoEl(nuevoNumeroDeNodosBajoEl);
 		return nodo;
 	}
-	NodoRB rotarIzquierdo(NodoRB nodo)
+	public NodoRB rotarIzquierdo(NodoRB nodo)
 	{
-	NodoRB x = nodo.darDerecho();
-	nodo.setDerecho(x.darIzquierdo());
-	x.setIzquierdo(nodo);
-	if(nodo.esRojo())
+		NodoRB x = nodo.darDerecho();
+		nodo.setDerecho(x.darIzquierdo());
+		x.setIzquierdo(nodo);
+		if(nodo.esRojo())
+		{
+			x.setRojo();
+		}
+		else
+		{
+			x.setNegro();
+		}
+		nodo.setRojo();
+		x.establecerNumNodosBajoEl(nodo.darNumNodosBajoEl());
+		nodo.establecerNumNodosBajoEl(1+ size(nodo.darIzquierdo()) + size(nodo.darDerecho()));
+		return x;
+	}
+
+	public NodoRB rotarDerecho(NodoRB nodo)
 	{
-		x.setRojo();
+		NodoRB x = nodo.darIzquierdo();
+		nodo.setIzquierdo(x.darDerecho());
+		x.setDerecho(nodo);
+		if(nodo.esRojo())
+		{
+			x.setRojo();
+		}
+		else
+		{
+			x.setNegro();
+		}
+		nodo.setRojo();
+		x.establecerNumNodosBajoEl(nodo.darNumNodosBajoEl());
+		nodo.establecerNumNodosBajoEl(1+ size(nodo.darIzquierdo()) + size(nodo.darDerecho()));
+		return x;
 	}
-	else
-	{
-		x.setNegro();
-	}
-	nodo.setRojo();
-	x.establecerNumNodosBajoEl(nodo.darNumNodosBajoEl());
-	nodo.establecerNumNodosBajoEl(1+ size(nodo.darIzquierdo()) + size(nodo.darDerecho()));
-	return x;
-	}
-	NodoRB rotarDerecho(NodoRB nodo)
-	{
-	NodoRB x = nodo.darIzquierdo();
-	nodo.setIzquierdo(x.darDerecho());
-	x.setDerecho(nodo);
-	if(nodo.esRojo())
-	{
-		x.setRojo();
-	}
-	else
-	{
-		x.setNegro();
-	}
-	nodo.setRojo();
-	x.establecerNumNodosBajoEl(nodo.darNumNodosBajoEl());
-	nodo.establecerNumNodosBajoEl(1+ size(nodo.darIzquierdo()) + size(nodo.darDerecho()));
-	return x;
-	}
+
 	void flipColores(NodoRB nodo)
 	{
-	nodo.setRojo(); 
-	nodo.darIzquierdo().setNegro();
-	nodo.darDerecho().setNegro();
+		nodo.setRojo(); 
+		nodo.darIzquierdo().setNegro();
+		nodo.darDerecho().setNegro();
 	}
 	@Override
 	public int height() {
-		// TODO Auto-generated method stub
-		return 0;
+		if (isEmpty()) return 0;
+		return height(raiz);
 	}
+	
+	private int height (NodoRB nodo) {
+		int rta = 1;
+		int alturaDerecha = 0;
+		int alturaIzquierda = 0;
+		if(nodo.tieneDerecho()) alturaDerecha = height(nodo.darDerecho());
+		if(nodo.tieneIzquierdo()) alturaIzquierda = height(nodo.darIzquierdo());
+		if(alturaDerecha>alturaIzquierda) rta += alturaDerecha;
+		else rta +=alturaIzquierda;
+		return rta;
+	}
+	
 	@Override
 	public int size() {
 		return size(raiz);
@@ -140,32 +156,71 @@ public class RBT<K extends Comparable<K>, V> implements TablaSimbolosOrdenada<K,
 	}
 	@Override
 	public K min() {
-		// TODO Auto-generated method stub
-		return null;
+		if (raiz==null) return null;
+		NodoRB act = raiz;
+		while (act.tieneIzquierdo()) act = act.darIzquierdo();
+		return (K) act.darKey();
 	}
 
 	@Override
 	public K max() {
-		// TODO Auto-generated method stub
-		return null;
+		if (raiz==null) return null;
+		NodoRB act = raiz;
+		while (act.tieneDerecho()) act = act.darDerecho();
+		return (K) act.darKey();
 	}
-
+	
 	@Override
 	public List<K> keySet() {
-		// TODO Auto-generated method stub
-		return null;
+		if (raiz==null) return null;
+		return keySet(raiz);
+	}
+	private List<K> keySet(NodoRB nodo) {
+		List<K> lista = new  ArrayList<K>();
+		lista.add((K)nodo.darKey());
+		if(nodo.tieneIzquierdo()) lista.addAll(keySet(nodo.darIzquierdo()));
+		if(nodo.tieneDerecho()) lista.addAll(keySet(nodo.darDerecho()));
+		return lista;
 	}
 
 	@Override
 	public List<K> keysInRange(K init, K end) {
-		// TODO Auto-generated method stub
-		return null;
+		if (raiz==null) return null;
+		return keysInRange(raiz, init, end);
+	}
+	
+	private List<K> keysInRange(NodoRB nodo, K init, K end) {
+		List<K> lista = new  ArrayList<K>();
+		if((((K) nodo.darKey()).compareTo(init)>=0) && (((K) nodo.darKey()).compareTo(end)<=0)) lista.add((K)nodo.darKey());
+		if(nodo.tieneIzquierdo()) lista.addAll(keysInRange(nodo.darIzquierdo(),init,end));
+		if(nodo.tieneDerecho()) lista.addAll(keysInRange(nodo.darDerecho(),init,end));
+		return lista;
 	}
 
 	@Override
 	public List<V> valuesInRange(K init, K end) {
-		// TODO Auto-generated method stub
-		return null;
+		if (raiz==null) return null;
+		return valuesInRange(raiz, init, end);
+	}
+	
+	private List<V> valuesInRange(NodoRB nodo, K init, K end) {
+		List<V> lista = new  ArrayList<V>();
+		if((((K) nodo.darKey()).compareTo(init)>=0) && (((K) nodo.darKey()).compareTo(end)<=0)) {
+			try {
+				ArrayList<V> definitiva = new ArrayList<V>();
+				ArrayList<Object> listaObjetos = (ArrayList<Object>) nodo.darValor();
+				for (Object object : listaObjetos) {
+					definitiva.add((V) object);
+				}
+				lista.addAll(definitiva);
+			}catch (Exception e) {
+				lista.add((V)nodo.darValor());
+			}
+		}
+			
+		if(nodo.tieneIzquierdo()) lista.addAll(valuesInRange(nodo.darIzquierdo(),init,end));
+		if(nodo.tieneDerecho()) lista.addAll(valuesInRange(nodo.darDerecho(),init,end));
+		return lista;
 	}
 
 }
--- tests/modeloEstructuraDatosTest/RBTTest.java
+++ tests/modeloEstructuraDatosTest/RBTTest.java
@@ -129,115 +129,117 @@ public class RBTTest {
 	public void getHeightTest() {
 		setUp1();
 		assertEquals(1,rbt1.getHeight(5));
+		assertEquals(3,rbt1.getHeight(1));
+		assertEquals(2,rbt1.getHeight(3));
+		assertEquals(2,rbt1.getHeight(8));
+		assertEquals(4,rbt1.getHeight(9));
+		assertEquals(3,rbt1.getHeight(10));
 		assertEquals(4,rbt1.getHeight(6));
-		assertEquals(5,rbt1.getHeight(7));
-		setUp3();
-		assertEquals(1,rbt2.getHeight("cinco"));
-		assertEquals(3,rbt2.getHeight("dos"));
-		assertEquals(4,rbt2.getHeight("diez"));
+		assertEquals(4,rbt1.getHeight(2));
+		assertEquals(4,rbt1.getHeight(0));
 	}
 	
-}	
-//	@Test
-//	public void containsTest() {
-//		setUp1();
-//		assertEquals(true,rbt1.contains(1));
-//		assertEquals(true,rbt1.contains(3));
-//		assertEquals(true,rbt1.contains(5));
-//		assertEquals(true,rbt1.contains(10));
-//		assertEquals(false,rbt1.contains(12));
-//		assertEquals(false,rbt1.contains(100));
-//		assertEquals(false,rbt1.contains(-1));
-//		assertEquals(false,rbt1.contains(30));
-//	}
-//	
-//	@Test
-//	public void heightTest() {
-//		setUp1();
-//		assertEquals(5,rbt1.height());
-//		setUp3();
-//		assertEquals(4,rbt2.height());
-//	}
-//	
-//	@Test
-//	public void minTest() {
-//		setUp1();
-//		assertEquals(0,rbt1.min());
-//		setUp3();
-//		assertEquals("cinco",rbt2.min());
-//		setUp2();
-//		assertEquals("cero",rbt2.min());
-//		setUp4();
-//		assertEquals(null,rbt1.min());
-//	}
-//	
-//	@Test
-//	public void maxTest() {
-//		setUp1();
-//		assertEquals(10,rbt1.max());
-//		setUp3();
-//		assertEquals("uno",rbt2.max());
-//		setUp2();
-//		assertEquals("uno",rbt2.max());
-//		setUp4();
-//		assertEquals(null,rbt1.max());
-//	}
-//	
-//	@Test
-//	public void keySetTest() {
-//		setUp1();
-//		assertEquals(true,rbt1.keySet().contains(1));
-//		assertEquals(true,rbt1.keySet().contains(10));
-//		assertEquals(11,rbt1.keySet().size());
-//		
-//		setUp2();
-//		assertEquals(true,rbt2.keySet().contains("uno"));
-//		assertEquals(true,rbt2.keySet().contains("cero"));
-//		assertEquals(11,rbt2.keySet().size());
-//		
-//		setUp3();
-//		assertEquals(true,rbt2.keySet().contains("uno"));
-//		assertEquals(true,rbt2.keySet().contains("diez"));
-//		assertEquals(5,rbt2.keySet().size());
-//		
-//		setUp4();
-//		assertEquals(null,rbt1.keySet());
-//		
-//	}
-//	
-//	@Test
-//	public void keysInRangeTest() {
-//		setUp1();
-//		assertEquals(true,rbt1.keysInRange(3,5).contains(4));
-//		assertEquals(true,rbt1.keysInRange(3,5).contains(3));
-//		assertEquals(false,rbt1.keysInRange(3,5).contains(1));
-//		assertEquals(false,rbt1.keysInRange(3,5).contains(10));
-//		assertEquals(false,rbt1.keysInRange(3,5).contains(6));
-//		assertEquals(5,rbt1.keysInRange(3,7).size());
-//	}
-//	
-//	@Test
-//	public void valuesInRange() {
-//		setUp1();
-//		assertEquals(true,rbt1.valuesInRange(3,5).contains("cuatro"));
-//		assertEquals(true,rbt1.valuesInRange(3,5).contains("cinco"));
-//		assertEquals(false,rbt1.valuesInRange(3,5).contains("uno"));
-//		assertEquals(false,rbt1.valuesInRange(3,5).contains("diez"));
-//		assertEquals(false,rbt1.valuesInRange(3,5).contains("seis"));
-//		assertEquals(6,rbt1.valuesInRange(3,7).size());
-//	}
-//	
-//
-//	@Test
-//	public void revisarConRepeticiones() {
-//		setUp6();
-//		assertEquals(1,rbt2.height());
-//		assertEquals(true,rbt2.valuesInRange(4,6).contains("1,5"));
-//		assertEquals(true,rbt2.valuesInRange(4,6).contains("3,5"));
-//		assertEquals(true,rbt2.valuesInRange(4,6).contains("5,5"));
-//		
-//	}
-//	
-//	
-//	
-//}
+	
+	@Test
+	public void containsTest() {
+		setUp1();
+		assertEquals(true,rbt1.contains(1));
+		assertEquals(true,rbt1.contains(3));
+		assertEquals(true,rbt1.contains(5));
+		assertEquals(true,rbt1.contains(10));
+		assertEquals(false,rbt1.contains(12));
+		assertEquals(false,rbt1.contains(100));
+		assertEquals(false,rbt1.contains(-1));
+		assertEquals(false,rbt1.contains(30));
+	}
+	
+	@Test
+	public void heightTest() {
+		setUp1();
+		assertEquals(4,rbt1.height());
+		setUp3();
+		assertEquals(3,rbt2.height());
+	}
+	
+	@Test
+	public void minTest() {
+		setUp1();
+		assertEquals(0,rbt1.min());
+		setUp3();
+		assertEquals("cinco",rbt2.min());
+		setUp2();
+		assertEquals("cero",rbt2.min());
+		setUp4();
+		assertEquals(null,rbt1.min());
+	}
+	
+	@Test
+	public void maxTest() {
+		setUp1();
+		assertEquals(10,rbt1.max());
+		setUp3();
+		assertEquals("uno",rbt2.max());
+		setUp2();
+		assertEquals("uno",rbt2.max());
+		setUp4();
+		assertEquals(null,rbt1.max());
+	}
+	
+	@Test
+	public void keySetTest() {
+		setUp1();
+		assertEquals(true,rbt1.keySet().contains(1));
+		assertEquals(true,rbt1.keySet().contains(10));
+		assertEquals(11,rbt1.keySet().size());
+		
+		setUp2();
+		assertEquals(true,rbt2.keySet().contains("uno"));
+		assertEquals(true,rbt2.keySet().contains("cero"));
+		assertEquals(11,rbt2.keySet().size());
+		
+		setUp3();
+		assertEquals(true,rbt2.keySet().contains("uno"));
+		assertEquals(true,rbt2.keySet().contains("diez"));
+		assertEquals(5,rbt2.keySet().size());
+		
+		setUp4();
+		assertEquals(null,rbt1.keySet());
+		
+	}
+	
+	@Test
+	public void keysInRangeTest() {
+		setUp1();
+		assertEquals(true,rbt1.keysInRange(3,5).contains(4));
+		assertEquals(true,rbt1.keysInRange(3,5).contains(3));
+		assertEquals(false,rbt1.keysInRange(3,5).contains(1));
+		assertEquals(false,rbt1.keysInRange(3,5).contains(10));
+		assertEquals(false,rbt1.keysInRange(3,5).contains(6));
+		assertEquals(5,rbt1.keysInRange(3,7).size());
+	}
+	
+	@Test
+	public void valuesInRange() {
+		setUp1();
+		assertEquals(true,rbt1.valuesInRange(3,5).contains("cuatro"));
+		assertEquals(true,rbt1.valuesInRange(3,5).contains("cinco"));
+		assertEquals(false,rbt1.valuesInRange(3,5).contains("uno"));
+		assertEquals(false,rbt1.valuesInRange(3,5).contains("diez"));
+		assertEquals(false,rbt1.valuesInRange(3,5).contains("seis"));
+		assertEquals(6,rbt1.valuesInRange(3,7).size());
+	}
+	
+
+	@Test
+	public void revisarConRepeticiones() {
+		setUp6();
+		assertEquals(1,rbt2.height());
+		assertEquals(true,rbt2.valuesInRange(4,6).contains("1,5"));
+		assertEquals(true,rbt2.valuesInRange(4,6).contains("3,5"));
+		assertEquals(true,rbt2.valuesInRange(4,6).contains("5,5"));
+		
+	}
+	
+	
+	
+}
