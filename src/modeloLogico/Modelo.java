acada5b26fcabd244b6f04e286540ad4e4b61150
Alejandro Ahogado <a.ahogado@uniandes.edu.co>
2020-11-17 00:51:51 -0500

    Min. avance carga

--- src/modeloLogico/Modelo.java
+++ src/modeloLogico/Modelo.java
@@ -13,14 +13,38 @@ import com.opencsv.CSVParserBuilder;
 import com.opencsv.CSVReader;
 import com.opencsv.CSVReaderBuilder;
 
+import modeloEstructuraDatos.Accidente;
+import modeloEstructuraDatos.RBT;
 import view.View;
 
 
 
 public class Modelo {
 	
+	public String RUTA_DATOS_1 = "./data/201801-1-citibike-tripdata.csv";
+	
+	public String RUTA_DATOS_2 = "./data/201801-2-citibike-tripdata.csv";
+	
+
+	private FileReader archivo;
+	private CSVReader lector;
+	private int cantidadDeViajesBicicletaCargados;
 	private View view; 
 	
 	public Modelo() { view = new View(); }
 	
+	public void cargarDatos(String ruta1, String ruta2 )
+	{
+		try {
+			cantidadDeViajesBicicletaCargados = 0;
+			long startTime = System.nanoTime();
+			CSVParser parser = new CSVParserBuilder().withSeparator(',').build();
+			tabla = DiGraph new <idEstacion, Viaje>();
+			archivo1 = new FileReader(ruta1);
+			archivo2 = new FileReader(ruta2);
+			lector = new CSVReaderBuilder (archivo1).withCSVParser(parser).build();
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+	}
 }
